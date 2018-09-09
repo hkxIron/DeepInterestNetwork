597b4bdebe2cbd768686de9e046afebd84d3a9cf
hukexin <hukexin0000@126.com>
2018-09-10 00:26:03 +0800

    看懂代码

--- din/input.py
+++ din/input.py
@@ -23,11 +23,12 @@ class DataInput:
     self.i += 1
 
     u, i, y, sl = [], [], [], []
+    # dataset里的每一行(reviewerID, hist_item_seq, pos_or_neg_item, label)
     for t in ts:
-      u.append(t[0])
-      i.append(t[2])
-      y.append(t[3])
-      sl.append(len(t[1]))
+      u.append(t[0]) # user_id
+      i.append(t[2]) # pos_or_neg_item
+      y.append(t[3]) # label
+      sl.append(len(t[1])) # history_item_seq_len
     max_sl = max(sl)
 
     hist_i = np.zeros([len(ts), max_sl], np.int64)
@@ -37,7 +38,7 @@ class DataInput:
       for l in range(len(t[1])):
         hist_i[k][l] = t[1][l]
       k += 1
-
+    # user_context(user_id), pos_or_neg_item, label, hist_item_seq, sequence_length
     return self.i, (u, i, y, hist_i, sl)
 
 class DataInputTest:
@@ -63,11 +64,12 @@ class DataInputTest:
     self.i += 1
 
     u, i, j, sl = [], [], [], []
+    # dataset里的每一行(reviewerID, hist_item_seq, (pos_item, neg_item))
     for t in ts:
-      u.append(t[0])
-      i.append(t[2][0]) # 就这里与DataInput有差别
-      j.append(t[2][1])
-      sl.append(len(t[1]))
+      u.append(t[0]) # reviewerID
+      i.append(t[2][0]) # pos_item
+      j.append(t[2][1]) # neg_item
+      sl.append(len(t[1])) # history_item_seq_length
     max_sl = max(sl)
 
     hist_i = np.zeros([len(ts), max_sl], np.int64)
--- din/model.py
+++ din/model.py
@@ -6,11 +6,11 @@ class Model(object):
 
   def __init__(self, user_count, item_count, cate_count, cate_list):
 
-    self.u = tf.placeholder(tf.int32, [None,]) # [B], user_behaviour
-    self.i = tf.placeholder(tf.int32, [None,]) # [B], candidate ad items
-    self.j = tf.placeholder(tf.int32, [None,]) # [B],
-    self.y = tf.placeholder(tf.float32, [None,]) # [B], label
-    self.history_item_sequence = tf.placeholder(tf.int32, [None, None]) # [B, T], 历史商品的序列
+    self.user_context = tf.placeholder(tf.int32, [None, ]) # [B], user_context
+    self.i = tf.placeholder(tf.int32, [None,]) # [B], candidate ad items,即待预测的商品item
+    self.j = tf.placeholder(tf.int32, [None,]) # [B], negative_items,只在evaluate时才会用到
+    self.label = tf.placeholder(tf.float32, [None, ]) # [B], label
+    self.history_item_sequence = tf.placeholder(tf.int32, [None, None]) # [B, T], 历史商品的序列,[batch,seq_length]
     self.sequence_length = tf.placeholder(tf.int32, [None, ]) # [B], 当前batch中的最长序列的长度
     self.lr = tf.placeholder(tf.float64, [])
 
@@ -18,67 +18,89 @@ class Model(object):
 
     user_emb_w = tf.get_variable("user_emb_w", [user_count, hidden_units])  # [user_count, hidden]
     item_emb_w = tf.get_variable("item_emb_w", [item_count, hidden_units // 2]) # [item_count, hidden/2]
-    item_b = tf.get_variable("item_b", [item_count],
-                             initializer=tf.constant_initializer(0.0))
+    item_b = tf.get_variable("item_b", [item_count], initializer=tf.constant_initializer(0.0)) # [item_count]
     cate_emb_w = tf.get_variable("cate_emb_w", [cate_count, hidden_units // 2]) # [cate_count, hidden/2]
-    cate_list = tf.convert_to_tensor(cate_list, dtype=tf.int64)
+    cate_list = tf.convert_to_tensor(cate_list, dtype=tf.int64) # 访问商品的类目id, 407*18
 
-    u_emb = tf.nn.embedding_lookup(user_emb_w, self.u) # [batch , hidden_units]
+    u_emb = tf.nn.embedding_lookup(user_emb_w, self.user_context) # [batch , hidden]
 
-    ic = tf.gather(cate_list, self.i) # gather与embedding_lookup类似,
-    i_emb = tf.concat(values = [
-        tf.nn.embedding_lookup(item_emb_w, self.i),
-        tf.nn.embedding_lookup(cate_emb_w, ic),
+    i_cate = tf.gather(cate_list, self.i) # gather与embedding_lookup类似, [batch, X]
+    i_emb = tf.concat(values = [ # batch* (hidden/2+ X) => [batch, hidden]
+        tf.nn.embedding_lookup(item_emb_w, self.i), # batch* hidden/2
+        tf.nn.embedding_lookup(cate_emb_w, i_cate), # batch* X，从后文中看，X = hidden/2
         ], axis=1)
-    i_b = tf.gather(item_b, self.i)
+    i_bias = tf.gather(item_b, self.i)  # [batch]
 
-    jc = tf.gather(cate_list, self.j)
-    j_emb = tf.concat([
-        tf.nn.embedding_lookup(item_emb_w, self.j),
-        tf.nn.embedding_lookup(cate_emb_w, jc),
-        ], axis=1)
-    j_b = tf.gather(item_b, self.j)
-
-    hc = tf.gather(cate_list, self.history_item_sequence)
-    h_emb = tf.concat([
-        tf.nn.embedding_lookup(item_emb_w, self.history_item_sequence),
-        tf.nn.embedding_lookup(cate_emb_w, hc),
+    # history_cate_seq: [batch, seq_len, X]
+    history_cate_seq = tf.gather(cate_list, self.history_item_sequence) # cate_list: cate_num*X, history_item_seq: batch*seq_len
+    # history_embed: [batch, seq_len, hidden]
+    history_embed = tf.concat([
+        tf.nn.embedding_lookup(item_emb_w, self.history_item_sequence), # [batch, seq_len, hidden/2]
+        tf.nn.embedding_lookup(cate_emb_w, history_cate_seq), # [batch, seq_len, hidden/2]
         ], axis=2)
 
-    hist =attention(i_emb, h_emb, self.sequence_length)
-    #-- attention end ---
-    
-    hist = tf.layers.batch_normalization(inputs = hist)
-    hist = tf.reshape(hist, [-1, hidden_units])
-    hist = tf.layers.dense(hist, hidden_units)
+    # history_attend: [batch,1,hidden]
+    user_history_attend = attention(i_emb, history_embed, self.sequence_length) # i_emb:[batch,hidden] , history_embed:[batch,seq_len, hidden]
 
-    u_emb = hist
-    print u_emb.get_shape().as_list()
-    print i_emb.get_shape().as_list()
-    print j_emb.get_shape().as_list()
+    #-- attention end ---
+    user_history_attend = tf.layers.batch_normalization(inputs = user_history_attend)
+    user_history_attend = tf.reshape(user_history_attend, [-1, hidden_units]) # [batch,hidden]
+    user_history_attend = tf.layers.dense(user_history_attend, hidden_units) # 接入一个全连接层，输出为 hidden, [batch,hidden]
+
+    u_emb = user_history_attend # [batch, hidden]
+    print(u_emb.get_shape().as_list())
+    print(i_emb.get_shape().as_list())
     #-- fcn begin -------
-    din_i = tf.concat([u_emb, i_emb], axis=-1)
+    # for i, 将user_embed, item_embed 连接起来,只用了user_id,history_item_id的信息，并未使用user的其它信息，应该可以加入进来
+    din_i = tf.concat([u_emb, i_emb], axis=-1) # [batch, hidden]
     din_i = tf.layers.batch_normalization(inputs=din_i, name='b1')
-    d_layer_1_i = tf.layers.dense(din_i, 80, activation=tf.nn.sigmoid, name='f1')
-    #if u want try dice change sigmoid to None and add dice layer like following two lines. You can also find model_dice.py in this folder.
+    d_layer_1_i = tf.layers.dense(din_i, units=80, activation=tf.nn.sigmoid, name='f1')
+    #if you want try dice change sigmoid to None and add dice layer like following two lines. You can also find model_dice.py in this folder.
     #d_layer_1_i = tf.layers.dense(din_i, 80, activation=None, name='f1')
     #d_layer_1_i = dice(d_layer_1_i, name='dice_1_i')
-    d_layer_2_i = tf.layers.dense(d_layer_1_i, 40, activation=tf.nn.sigmoid, name='f2')
+    d_layer_2_i = tf.layers.dense(d_layer_1_i, units=40, activation=tf.nn.sigmoid, name='f2')
     #d_layer_2_i = dice(d_layer_2_i, name='dice_2_i')
-    d_layer_3_i = tf.layers.dense(d_layer_2_i, 1, activation=None, name='f3')
-    din_j = tf.concat([u_emb, j_emb], axis=-1)
+    d_layer_3_i = tf.layers.dense(d_layer_2_i, 1, activation=None, name='f3') # [batch, 1]
+    d_layer_3_i = tf.reshape(d_layer_3_i, [-1]) # [batch]
+
+    self.logits = i_bias + d_layer_3_i # [batch], 最后的logits，用来计算交叉熵损失函数
+    self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.label))
+
+    # Step variable
+    self.global_step = tf.Variable(0, trainable=False, name='global_step')
+    self.global_epoch_step = tf.Variable(0, trainable=False, name='global_epoch_step')
+    self.global_epoch_step_op = tf.assign(self.global_epoch_step, self.global_epoch_step+1)
+
+    trainable_params = tf.trainable_variables()
+    self.opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
+    gradients = tf.gradients(self.loss, trainable_params)
+    clip_gradients, _ = tf.clip_by_global_norm(gradients, 5)
+    self.train_op = self.opt.apply_gradients(zip(clip_gradients, trainable_params), global_step=self.global_step)
+
+    # --------------------
+    # for j
+    j_cate = tf.gather(cate_list, self.j) #  [batch, X]
+    j_emb = tf.concat([ # batch* (hidden/2+ X)
+        tf.nn.embedding_lookup(item_emb_w, self.j),
+        tf.nn.embedding_lookup(cate_emb_w, j_cate),
+    ], axis=1)
+
+    print(j_emb.get_shape().as_list())
+
+    j_bias = tf.gather(item_b, self.j) # [batch]
+    din_j = tf.concat([u_emb, j_emb], axis=-1) # [batch, hidden+hidden]
     din_j = tf.layers.batch_normalization(inputs=din_j, name='b1', reuse=True)
-    d_layer_1_j = tf.layers.dense(din_j, 80, activation=tf.nn.sigmoid, name='f1', reuse=True)
+    d_layer_1_j = tf.layers.dense(din_j, units=80, activation=tf.nn.sigmoid, name='f1', reuse=True)
     #d_layer_1_j = dice(d_layer_1_j, name='dice_1_j')
-    d_layer_2_j = tf.layers.dense(d_layer_1_j, 40, activation=tf.nn.sigmoid, name='f2', reuse=True)
+    d_layer_2_j = tf.layers.dense(d_layer_1_j, units=40, activation=tf.nn.sigmoid, name='f2', reuse=True)
     #d_layer_2_j = dice(d_layer_2_j, name='dice_2_j')
-    d_layer_3_j = tf.layers.dense(d_layer_2_j, 1, activation=None, name='f3', reuse=True)
-    d_layer_3_i = tf.reshape(d_layer_3_i, [-1])
-    d_layer_3_j = tf.reshape(d_layer_3_j, [-1])
-    x = i_b - j_b + d_layer_3_i - d_layer_3_j # [B]
-    self.logits = i_b + d_layer_3_i
-    u_emb_all = tf.expand_dims(u_emb, 1)
-    u_emb_all = tf.tile(u_emb_all, [1, item_count, 1])
+    d_layer_3_j = tf.layers.dense(d_layer_2_j, units=1, activation=None, name='f3', reuse=True) # [batch, 1]
+    d_layer_3_j = tf.reshape(d_layer_3_j, [-1]) # [batch]
+
+    x = i_bias - j_bias + d_layer_3_i - d_layer_3_j # [B]
+    u_emb_all = tf.expand_dims(u_emb, 1) # [batch,1,hidden]
+    u_emb_all = tf.tile(u_emb_all, [1, item_count, 1]) # [batch, item_count, hidden]
+
     # logits for all item:
     all_emb = tf.concat([
         item_emb_w,
@@ -88,60 +110,38 @@ class Model(object):
     all_emb = tf.tile(all_emb, [512, 1, 1])
     din_all = tf.concat([u_emb_all, all_emb], axis=-1)
     din_all = tf.layers.batch_normalization(inputs=din_all, name='b1', reuse=True)
-    d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1', reuse=True)
+    d_layer_1_all = tf.layers.dense(din_all, units=80, activation=tf.nn.sigmoid, name='f1', reuse=True)
     #d_layer_1_all = dice(d_layer_1_all, name='dice_1_all')
-    d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2', reuse=True)
+    d_layer_2_all = tf.layers.dense(d_layer_1_all, units=40, activation=tf.nn.sigmoid, name='f2', reuse=True)
     #d_layer_2_all = dice(d_layer_2_all, name='dice_2_all')
-    d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3', reuse=True)
+    d_layer_3_all = tf.layers.dense(d_layer_2_all, units=1, activation=None, name='f3', reuse=True)
     d_layer_3_all = tf.reshape(d_layer_3_all, [-1, item_count])
     self.logits_all = tf.sigmoid(item_b + d_layer_3_all)
     #-- fcn end -------
 
-    
     self.mf_auc = tf.reduce_mean(tf.to_float(x > 0))
-    self.score_i = tf.sigmoid(i_b + d_layer_3_i)
-    self.score_j = tf.sigmoid(j_b + d_layer_3_j)
+    self.score_i = tf.sigmoid(i_bias + d_layer_3_i)
+    self.score_j = tf.sigmoid(j_bias + d_layer_3_j)
     self.score_i = tf.reshape(self.score_i, [-1, 1])
     self.score_j = tf.reshape(self.score_j, [-1, 1])
-    self.p_and_n = tf.concat([self.score_i, self.score_j], axis=-1)
-    print self.p_and_n.get_shape().as_list()
-
-
-    # Step variable
-    self.global_step = tf.Variable(0, trainable=False, name='global_step')
-    self.global_epoch_step = \
-        tf.Variable(0, trainable=False, name='global_epoch_step')
-    self.global_epoch_step_op = \
-        tf.assign(self.global_epoch_step, self.global_epoch_step+1)
-
-    self.loss = tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.logits,
-            labels=self.y)
-        )
-
-    trainable_params = tf.trainable_variables()
-    self.opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
-    gradients = tf.gradients(self.loss, trainable_params)
-    clip_gradients, _ = tf.clip_by_global_norm(gradients, 5)
-    self.train_op = self.opt.apply_gradients(
-        zip(clip_gradients, trainable_params), global_step=self.global_step)
-
+    self.positive_and_negative = tf.concat([self.score_i, self.score_j], axis=-1)
+    print(self.positive_and_negative.get_shape().as_list())
 
   def train(self, sess, uij, l):
+    # uij: user_context, pos_or_neg_item(candidate_item), label, history_item_seq, sequence_length
     loss, _ = sess.run([self.loss, self.train_op], feed_dict={
-        self.u: uij[0],
-        self.i: uij[1],
-        self.y: uij[2],
-        self.history_item_sequence: uij[3],
-        self.sequence_length: uij[4],
+        self.user_context: uij[0], # user_id
+        self.i: uij[1], # candidate_item`
+        self.label: uij[2], # label
+        self.history_item_sequence: uij[3], # history_item_sequence
+        self.sequence_length: uij[4], # sequence_length
         self.lr: l,
         })
     return loss
 
   def eval(self, sess, uij):
-    u_auc, socre_p_and_n = sess.run([self.mf_auc, self.p_and_n], feed_dict={
-        self.u: uij[0],
+    u_auc, socre_p_and_n = sess.run([self.mf_auc, self.positive_and_negative], feed_dict={
+        self.user_context: uij[0],
         self.i: uij[1],
         self.j: uij[2],
         self.history_item_sequence: uij[3],
@@ -151,7 +151,7 @@ class Model(object):
 
   def test(self, sess, uid, hist_i, sl):
     return sess.run(self.logits_all, feed_dict={
-        self.u: uid,
+        self.user_context: uid,
         self.history_item_sequence: hist_i,
         self.sequence_length: sl,
         })
@@ -172,33 +172,39 @@ def extract_axis_1(data, ind):
 
 def attention(queries, keys, keys_length):
   '''
-    queries:     [B, H]
+    queries:     [B, H], B:batch, H:hidden, T:seq_length
     keys:        [B, T, H]
     keys_length: [B]
   '''
   queries_hidden_units = queries.get_shape().as_list()[-1]
-  queries = tf.tile(queries, [1, tf.shape(keys)[1]])
-  queries = tf.reshape(queries, [-1, tf.shape(keys)[1], queries_hidden_units])
-  din_all = tf.concat([queries, keys, queries-keys, queries*keys], axis=-1)
+  queries = tf.tile(queries, [1, tf.shape(keys)[1]]) # [batch, hidden*seq_length]
+  queries = tf.reshape(queries, [-1, tf.shape(keys)[1], queries_hidden_units]) # [batch, seq_length, hidden]
+  # queries * keys: 是内积吧，而不是论文中所说的外积
+  din_all = tf.concat([queries, keys, queries-keys, queries*keys], axis=-1) # [batch,seq_length, hidden+hidden+hidden+hidden]
   d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1_att')
   d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2_att')
-  d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3_att')
-  d_layer_3_all = tf.reshape(d_layer_3_all, [-1, 1, tf.shape(keys)[1]])
-  outputs = d_layer_3_all 
+  d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3_att') # [batch, seq_length, 1]
+  d_layer_3_all = tf.reshape(d_layer_3_all, [-1, 1, tf.shape(keys)[1]]) # [batch, 1, seq_length]
+  outputs = d_layer_3_all  # [batch, 1, seq_length]
   # Mask
-  key_masks = tf.sequence_mask(keys_length, tf.shape(keys)[1])   # [B, T]
-  key_masks = tf.expand_dims(key_masks, 1) # [B, 1, T]
-  paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
-  outputs = tf.where(key_masks, outputs, paddings)  # [B, 1, T]
+  # 将batch中的每一行都填充成seq_length的长度，其中true的个数为keys_length中的值
+  key_masks = tf.sequence_mask(keys_length, tf.shape(keys)[1])   # [B, T], keys_length:[batch], tf.shape[keys][1]: seq_length
+  key_masks = tf.expand_dims(key_masks, 1) # [batch, 1, seq_length]
+  paddings = tf.ones_like(outputs) * (-2 ** 32 + 1) # 全填成最小值
+  outputs = tf.where(key_masks, outputs, paddings)  # [B, 1, T], 条件选择，true的话，选择outputs,false则选择paddings,感觉这里用 bool_mask 就行了吧
 
   # Scale
-  outputs = outputs / (keys.get_shape().as_list()[-1] ** 0.5)
+  outputs = outputs / (keys.get_shape().as_list()[-1] ** 0.5) # output/(hidden_size**0.5)
 
   # Activation
-  outputs = tf.nn.softmax(outputs)  # [B, 1, T]
+  weigths = tf.nn.softmax(outputs)  # [B, 1, T], 在时间维度上进行softmax选择,输出权重
 
   # Weighted sum
-  outputs = tf.matmul(outputs, keys)  # [B, 1, H]
+  # weigths:[batch, 1, seq_length],
+  # keys:[batch, seq_length, hidden]
+  # weighted outputs: [batch, 1, hidden]
+  weighted_outputs = tf.matmul(weigths, keys)  # [B, 1, H], 3-d矩阵相乘，就是第0维要相同，去掉第0维后，里面都是普通二级矩阵相乘, 在time_length维度进行相乘及加权求和，非常巧妙,妙！
 
-  return outputs
+  # weighted outputs: [batch, 1, hidden]
+  return weighted_outputs
 
